28997e98e8d06ba696d25268697341bd96f32eea
needsleep772 <chen.andrew772@gmail.com>
2021-11-24 09:59:11 -0800

    added style css and modified routes added basic form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 8007;
 const app = express();
+const fs = require("fs").promises;
 
 // Don't worry about these 4 lines below
 app.set("view engine", "ejs");
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("createcard");
 });
 app.get("/people/:id", (req, res) => {
   res.render("people");
@@ -17,6 +18,34 @@ app.get("/people/:id", (req, res) => {
 
 app.get("/:id/photos", (req, res) => {
   const id = req.params.id;
+});
+
+app.get("/homepage", (req, res) => {
+  res.render("homepage");
+});
+
+app.post("/", (req, res) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./database.json", "utf-8")
+    .then((data) => {
+      console.log(req.body)
+      original_file = JSON.parse(data)
+      original_file["users"].push(
+        {
+          "id": "54az3",
+          "fullName":req.body.fullName,
+          "aboutMe": req.body.aboutMe,
+          "knownTechnologies": [],
+          "githubUrl": req.body.githubUrl,
+          "twitterUrl": req.body.twitterUrl,
+          "favoriteBooks": [],
+          "favoriteArtists": []
+        }
+      )
+      console.log(original_file["users"])
+    })
+    .catch((err) => reject(err));
+})
 });
 
 app.listen(PORT, () => {
